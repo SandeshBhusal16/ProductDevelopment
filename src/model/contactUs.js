2bd5ccaf69683cea814ccb1ccc3344080fba3af5
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2024-12-11 21:18:59 +0545

    feat: gitignore and contactus

--- src/model/contactUs.js
+++ src/model/contactUs.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-
+// Customers will be asked to
+// provide their name, email address, phone number, company name, country, job title, and job details
+// using a Contact Us form
 const ContactSchema = new mongoose.Schema({
   name: {
     type: String,
